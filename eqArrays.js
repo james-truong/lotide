adbabbad892ae3b02d174a573fe05d1b2271d3a5
james-truong <james.truong@hotmail.com>
2019-10-15 20:46:35 +0000

    added eqArray

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+function eqArrays(array1, array2){
+  if(array1.length !== array2.length){
+    return false;
+  }
+  for (let i = 0; i<array1.length; i++){
+    if(array1[i] !== array2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
