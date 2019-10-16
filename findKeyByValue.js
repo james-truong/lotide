414f557309150bfda19704e8a147f2f1ff9c006d
james-truong <james.truong@hotmail.com>
2019-10-16 17:28:15 +0000

    added keyfind

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+function findKeyByValue(object, value){
+  let keys = Object.keys(object);
+  for (key of keys){
+    if(object[key] === value){
+      return key;
+    }
+  }
+}
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
