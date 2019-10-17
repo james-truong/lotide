1a0f367ef4887d80525bf5fe08ba934db67badf6
james-truong <james.truong@hotmail.com>
2019-10-17 18:55:27 +0000

    takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const takeUntil = function(array, callback) {
+  // ...
+  let results = [];
+  for(item of array){
+    if(callback(item)){
+      break;
+    }
+    results.push(item);
+  }
+  return results;
+}
+
+function eqArrays(array1, array2){
+  if(array1.length !== array2.length){
+    return false;
+  }
+  for (let i = 0; i<array1.length; i++){
+    if(array1[i] !== array2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${array1}] !==  [${array2}]`);
+  }
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1,2,5,7,2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
+
