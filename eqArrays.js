d0b0d0082c3c2af7e9637a36bcaf1b4b9243e583
james-truong <james.truong@hotmail.com>
2019-10-18 19:12:56 +0000

    added eqobject recursive compare

--- eqArrays.js
+++ eqArrays.js
@@ -12,6 +12,10 @@ function eqArrays(array1, array2){
     return false;
   }
   for (let i = 0; i<array1.length; i++){
+    if(Array.isArray(array1[i])) {
+      console.log(array1[i+1]);
+      return eqArrays(array1[i+1], array2[i+1]);
+    }
     if(array1[i] !== array2[i]){
       return false;
     }
@@ -22,3 +26,8 @@ function eqArrays(array1, array2){
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+eqArrays([[2, 3], [4]], [[2, 3], [4]]) // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+console.log(eqArrays([[2, 3], 4], [[2, 3], 4])); // => false
