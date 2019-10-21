d83bcadc92d59d40632f77c3574071baaec6e74d
james-truong <james.truong@hotmail.com>
2019-10-21 17:50:24 +0000

    refactored core lotide

--- middle.js
+++ middle.js
@@ -1,43 +1,8 @@
-function eqArrays(array1, array2){
-  if(array1.length !== array2.length){
-    return false;
-  }
-  for (let i = 0; i<array1.length; i++){
-    if(array1[i] !== array2[i]){
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅ Assertion Passed: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: [${array1}] !==  [${array2}]`);
-  }
-};
-
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
 
-function eqArrays(array1, array2){
-  if(array1.length !== array2.length){
-    return false;
-  }
-  for (let i = 0; i<array1.length; i++){
-    if(array1[i] !== array2[i]){
-      return false;
-    }
-  }
-  return true;
-}
 
 function middle(array){
   let middle = [];
@@ -58,5 +23,4 @@ function middle(array){
 
 // TEST CODE
 
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-
+module.exports = middle;
